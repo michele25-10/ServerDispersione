a58c05e3355cf57d71cee5e489b2ee3f206c43f0
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-05-17 13:09:05 +0200

    cambiamento colori

--- pages/getInfo.php
+++ pages/getInfo.php
@@ -46,7 +46,7 @@ error_reporting(0);
                             <th>Data</th>
                             <th>Note</th>
                             <th>Aula</th>
-                            <th>Opzione</th>
+                            <th>Opzioni</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -56,7 +56,7 @@ error_reporting(0);
                                 <td><?php echo $row['note'] ?></td>
                                 <td><?php echo $row['aula'] ?></td>
                                 <td>
-                                    <button id="edit" class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="onClick(<?php echo $row['id_incontro'] ?>)">Edit</button>
+                                    <button id="edit" class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="onClick(<?php echo $row['id_incontro'] ?>)">Modifica</button>
                                     <button class="btn btn-success me-3" onclick="window.location.href='presenze.php?id_incontro=<?php echo $row['id_incontro'] ?>&nome_corso=<?php echo $_GET['nome_corso'] ?>';" <?php if ($list_studenti == -1) {
                                                                                                                                                                                                                             echo "disabled";
                                                                                                                                                                                                                         } ?>>
@@ -163,8 +163,7 @@ error_reporting(0);
                         <th>Nome</th>
                         <th>Cognome</th>
                         <th>CF</th>
-                        <th>Opzione</th>
-                    </tr>
+                   </tr>
                 </thead>
                 <tbody>
                     <?php foreach ($list_studenti as $row) : ?>
