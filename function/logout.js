1d2f579623c254140cf70a7859b72ac1a54d7dbe
michele25-10 <michele.gabri01@gmail.com>
2023-05-26 10:18:33 +0200

    Logout e salvataggio nella sessione funzionante

---
+++ function/logout.js
@@ -0,0 +1,3 @@
+
+sessionStorage.clear();
+window.location.replace("https://dispersione.violamarchesini.it/index.php");
--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link rel="stylesheet" href="assets/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="assets/img/logo.svg">
 </head>
 
@@ -20,11 +19,9 @@
         </div>
         <h1 class="h3 mb-3 fw-bold">Inserisci le credenziali</h1>
         <label for="inputEmail" class="sr-only mb-2">Email</label>
-        <input type="email" id="inputEmail" class="form-control mb-4" placeholder="email" name="email" required
-            autofocus>
+        <input type="email" id="inputEmail" class="form-control mb-4" placeholder="email" name="email" required autofocus>
         <label for="inputPassword" class="sr-only mb-2">Password</label>
-        <input type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" name="password"
-            required>
+        <input type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" name="password" required>
 
         <?php
         session_start();
@@ -47,9 +44,9 @@
                 if ($res == -1) {
                     echo ('<p class=text-danger>Email o password errata</p>');
                 } else {
-                    $_SESSION['user_id'] = $res;
-                    //header('Location: https://dispersione.violamarchesini.it/pages/homepage.php');
-                    echo '<script>window . location . replace("https://dispersione.violamarchesini.it/pages/homepage.php");</script>';
+                    echo '<script>
+                    sessionStorage.setItem("user_id", "' . $res . '");
+                    window . location . replace("https://dispersione.violamarchesini.it/pages/homepage.php");</script>';
                 }
             } else {
                 echo ('<p class="text-danger">Campo richiesto</p>');
@@ -62,66 +59,65 @@
         </div>
     </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 </body>
 
 </html>
 
 <style>
-html,
-body {
-    height: 100%;
-}
+    html,
+    body {
+        height: 100%;
+    }
 
-body {
-    display: -ms-flexbox;
-    display: -webkit-box;
-    display: flex;
-    -ms-flex-align: center;
-    -ms-flex-pack: center;
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    padding-top: 40px;
-    padding-bottom: 40px;
-    background-color: #f5f5f5;
-}
+    body {
+        display: -ms-flexbox;
+        display: -webkit-box;
+        display: flex;
+        -ms-flex-align: center;
+        -ms-flex-pack: center;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+    }
 
-.form-signin {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
-    margin: 0 auto;
-}
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+    }
 
-.form-signin .checkbox {
-    font-weight: 400;
-}
+    .form-signin .checkbox {
+        font-weight: 400;
+    }
 
-.form-signin .form-control {
-    position: relative;
-    box-sizing: border-box;
-    height: auto;
-    padding: 10px;
-    font-size: 16px;
-}
+    .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+    }
 
-.form-signin .form-control:focus {
-    z-index: 2;
-}
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
 
-.form-signin input[type="email"] {
-    margin-bottom: -1px;
-    /* border-bottom-right-radius: 0; */
-    /* border-bottom-left-radius: 0; */
-}
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        /* border-bottom-right-radius: 0; */
+        /* border-bottom-left-radius: 0; */
+    }
 
-.form-signin input[type="password"] {
-    margin-bottom: 10px;
-    /* border-top-left-radius: 0; */
-    /* border-top-right-radius: 0; */
-}
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        /* border-top-left-radius: 0; */
+        /* border-top-right-radius: 0; */
+    }
 </style>
