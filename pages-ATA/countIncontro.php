dc13dd20a3d3465e6875f28d8440286d9adf2fb3
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-06-07 22:31:56 +0200

    ultimata pages ata

--- pages-ATA/countIncontro.php
+++ pages-ATA/countIncontro.php
@@ -19,41 +19,52 @@
     $list_incontri = countIncontro();
     ?>
 
-    <div class="container mt-5">
+<div class="container mt-5">
         <?php echo ('<br>
     <h2>Studenti che partecipano ai corsi dei prossimi 15 giorni:</h2>
     <br>');
         ?>
-        <?php if ($list_incontri != -1): ?>
-        <table id="example" class="display" style="width:100%">
-            <thead>
-                <tr>
-                    <th>Data</th>
-                    <th>Numero Studenti</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($list_incontri as $row): ?>
-                <tr>
-                    <td>
-                        <?php echo $row['data'] ?>
-                    </td>
-                    <td>
-                        <?php echo $row['partecipanti'] ?>
-                    </td>
-                </tr>
-                <?php endforeach ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th>Data</th>
-                    <th>Studenti</th>
-                </tr>
-            </tfoot>
-        </table>
+        <?php if ($list_incontri != -1) : ?>
+            <table id="example" class="display" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Numero Studenti</th>
+                        <th> Opzioni </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($list_incontri as $row) : ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['data'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['partecipanti'] ?>
+                            </td>
+                            <td>
+                                <a href="getInfoStudenti.php?data=<?php echo $row['data'] ?>&nome_corso=<?php echo $row['nome_corso'] ?>">
+                                    <button class="btn btn-success">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                                        </svg>
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Data</th>
+                        <th>Studenti</th>
+                        <th>Opzioni</th>
+                    </tr>
+                </tfoot>
+            </table>
         <?php endif ?>
-        <?php if ($list_incontri == -1): ?>
-        <h2 class="text-danger">Non ci sono incontri per i prossimi 15 giorni!</h2>
+        <?php if ($list_incontri == -1) : ?>
+            <h2 class="text-danger">Non ci sono incontri per i prossimi 15 giorni!</h2>
         <?php endif ?>
     </div>
 
